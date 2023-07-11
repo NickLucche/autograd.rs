7660ec0536f7a1784fa73b0811a6c02b6f7d87b8
NickLucche <nicolo.lucchesi@gmail.com>
2023-07-11 18:43:02 +0200

    wip bwd

--- src/autograd/autograd.rs
+++ src/autograd/autograd.rs
@@ -1,6 +1,6 @@
+use std::cell::RefCell;
 use std::ops;
 use std::rc::Rc;
-use std::cell::RefCell;
 
 // TODO actual tensor lib for data
 // TODO this thing might as well go on heap, since data itself does and use Box<Tensor> around
@@ -9,9 +9,9 @@ pub struct Tensor {
     // TODO rename to graph_node
     // graph: Option<&'graph dyn Operator>
     // I want interior mutability to set only the graph ref, not the rest
-    graph: Option< Rc<RefCell<Node>> >,
+    graph: Option<Rc<RefCell<Node>>>,
     // graph: Option< Rc<Node> >,
-    data: Vec<i32>,
+    pub data: Vec<i32>,
     grad: Vec<i32>,
 }
 impl Tensor {
@@ -20,7 +20,7 @@ impl Tensor {
             requires_grad: false,
             graph: None,
             data: vec![0; 10],
-            grad: vec![0; 10]
+            grad: vec![0; 10],
         }
     }
 }
@@ -48,14 +48,14 @@ struct Node {
     // TODO an operator doesn't need a grad cache it can just forward it, the variable does
     pub grad: Tensor,
     // TODO swap with Tensor
-    pub variables: Vec< Rc<RefCell<Tensor>> >,
-    pub parents: Option< Vec<Rc<RefCell<Node>>> >,
+    pub variables: Vec<Rc<RefCell<Tensor>>>,
+    pub parents: Option<Vec<Rc<RefCell<Node>>>>,
 }
 
 impl Node {
     fn new(
         name: OperatorNodes,
-        variables: Vec< Rc<RefCell<Tensor>>>,
+        variables: Vec<Rc<RefCell<Tensor>>>,
         parents: Option<Vec<Rc<RefCell<Node>>>>,
     ) -> Self {
         Node {
@@ -82,39 +82,53 @@ impl Node {
 
 pub struct ReLU;
 // TODO backward algo
-fn ones()->Tensor {
+fn ones() -> Tensor {
     Tensor::new()
 }
-// fn backward_algo(node: Node, grad: Option<Tensor>) {
-//     let grad = grad.unwrap_or(ones());
-//     // TODO from trait node.name->operator
-//     let op = ReLU{};
-//     op.backward(x)
-//     for parent in node.parents {
-        
-//     }
-// }
+fn backward_algo(node: Node, prev_grad: Option<Tensor>) {
+    let prev_grad = prev_grad.unwrap_or(ones());
+    // 1. compute gradient(s) of current operator wrt its input(s)
+    // TODO from trait node.name->operator
+    let op = ReLU {};
+    // TODO avoid computing grad altogheter if var does not require it
+    let grads = op.backward(node.variables, prev_grad);
+    // 2. accumulate gradient on input vars
+    for (i, var) in node.variables.iter().enumerate() {
+        // var.borrow_mut().grad += grads[i];
+        if var.borrow().requires_grad {
+            let mut acc = var.borrow_mut().grad;
+            for j in 0..acc.len() {
+                acc[j] += grads[i].data[j];
+            }
+        }
+    }
+    // 3. recurse on parent nodes
+    for parent in node.parents.unwrap_or(vec![]) {
+        backward_algo(*parent.borrow(), Some(grads[0]));
+    }
+}
 pub trait Operator {
-    fn forward(&self, xs: Vec< Rc<RefCell<Tensor>> >) -> Tensor;
-    fn backward(&self, x: &Tensor, grad: Tensor) -> Tensor;
+    fn forward(&self, xs: Vec<Rc<RefCell<Tensor>>>) -> Tensor;
+    fn backward(&self, xs: Vec<Rc<RefCell<Tensor>>>, grad: Tensor) -> Vec<Tensor>;
     // fn accumulate_grad(&self, grad: Tensor) -> Tensor;
     // fn get_grad(&self) -> Tensor;
 
     // fn get_parents(&self)->Vec<Tensor>;
 }
-// TODO alias refcell rc node type
+// TODO solve this variable ordering/naming problem
 impl<'a> Operator for ReLU {
-    fn backward(&self, x: &Tensor, mut grad: Tensor) -> Tensor {
+    fn backward(&self, xs: Vec<Rc<RefCell<Tensor>>>, mut grad: Tensor) -> Vec<Tensor> {
         // having grad as input allows to avoid unnecessary allocations
+        let x = xs[0].borrow();
         for i in 0..x.data.len() {
             if x.data[i] <= 0 {
                 grad.data[i] = 0;
             }
         }
-        grad
+        vec![grad]
     }
-    fn forward(&self, xs: Vec< Rc<RefCell<Tensor>> >) -> Tensor {
-        // **** 
+    fn forward(&self, xs: Vec<Rc<RefCell<Tensor>>>) -> Tensor {
+        // ****
         // TODO this code must be shared or called explicitely
 
         // keep references to operator inputs
@@ -130,14 +144,18 @@ impl<'a> Operator for ReLU {
         // instatiate new operator node on heap
         let mut op: Rc<RefCell<Node>> = if op_parents.len() > 0 {
             // attach to graph by linking its parents!
-            Rc::new(RefCell::new(Node::new(OperatorNodes::ReLU, vars, Some(op_parents))))
+            Rc::new(RefCell::new(Node::new(
+                OperatorNodes::ReLU,
+                vars,
+                Some(op_parents),
+            )))
         } else {
             // first node in the graph
             Rc::new(RefCell::new(Node::new(OperatorNodes::ReLU, vars, None)))
         };
 
         // TODO we could attach it to out tensor if op not in_place, do if on that
-        for x in xs {
+        for x in &xs {
             if x.borrow().graph.is_none() {
                 // init new graph!
                 (*x).borrow_mut().graph = Some(Rc::clone(&op));
@@ -149,11 +167,11 @@ impl<'a> Operator for ReLU {
         // ****
 
         let mut t = Tensor::new();
-        // for (i, val) in xs[0].data.iter().enumerate() {
-        //     if *val > 0 {
-        //         t.data[i] = *val;
-        //     }
-        // }
+        for (i, val) in xs[0].borrow_mut().data.iter().enumerate() {
+            if *val > 0 {
+                t.data[i] = *val;
+            }
+        }
         t
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 fn main() {
-    let mut x = Tensor::new();
+    let mut x: Tensor = Tensor::new();
+    x.data[0] = 69;
     let mut xs = vec![Rc::new(RefCell::new(x))];
-    ReLU{}.forward(xs);
-    println!("Hello, world!");
+    let res = ReLU{}.forward(xs);
+    for x in &res.data {
+        print!("{}\t", x);
+    }
 }
